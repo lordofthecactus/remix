d13faa36aba3a8f40ad6a3118bf70231976f80a2
Matt Brophy <matt@brophy.org>
2022-11-28 18:49:24 -0500

    RRR Step 2 - Server Rendering (#4669)
    * First pass at SSR using react-router-dom@6.4
    * useLoaderData/useActionData generics + useTransition/useFetcher types
    * Remove duped router in favor of 1.0.4-pre.0

--- packages/remix-server-runtime/routeModules.ts
+++ packages/remix-server-runtime/routeModules.ts
@@ -1,10 +1,9 @@
 import type { ComponentType } from "react";
-import type { Location } from "react-router-dom";
+import type { Location, Params } from "react-router-dom";
 
 import type { AppLoadContext, AppData } from "./data";
 import type { LinkDescriptor } from "./links";
 import type { RouteData } from "./routeData";
-import type { Params } from "./router";
 import type { Route } from "./routes";
 import type { SerializeFrom } from "./serialize";
 
